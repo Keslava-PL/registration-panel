dc561138807067e4aa5971602078c27ce9971ee7
Keslava-PL <kamdob1995@gmail.com>
2023-01-10 12:35:56 +0100

    added code formatting - all done

--- main.js
+++ main.js
@@ -1,61 +1,51 @@
+const loginInput = document.querySelector("#login");
+const passwordInput = document.querySelector("#password");
+const sUButton = document.querySelector("#strenghtenButton");
+const sUButton2 = document.querySelector("#strenghtenButton2");
+const sUButton3 = document.querySelector("#strenghtenButton3");
+const signUpButton = document.querySelector("#signUpButton");
 
-const loginInput = document.querySelector('#login');
-const passwordInput = document.querySelector('#password');
-const sUButton = document.querySelector('#strenghtenButton');
-const sUButton2 = document.querySelector('#strenghtenButton2');
-const sUButton3 = document.querySelector('#strenghtenButton3');
-const signUpButton =document.querySelector('#signUpButton');
+signUpButton.setAttribute("disabled", "");
+sUButton.setAttribute("hidden", "");
+sUButton2.setAttribute("hidden", "");
+sUButton3.setAttribute("hidden", "");
 
-signUpButton.setAttribute('disabled','');
-//sUButton.setAttribute('hidden','');
-//sUButton2.setAttribute('hidden','');
-//sUButton3.setAttribute('hidden','');
-
-function checkPassword(password){
-
+function checkPassword(password) {
   var upperCase = upperCaseCheck(password);
   var digit = digitCheck(password);
-  if(password.length >= 12 
-    && upperCase === true
-    && digit === true){
-      sUButton.removeAttribute('hidden','');
-      sUButton2.removeAttribute('hidden','');
-      sUButton3.removeAttribute('hidden','');
-  }
-  else if(password.length > 7 
-    && password.length < 12
-    && upperCase === true){
-
-      sUButton.removeAttribute('hidden','');
-      sUButton2.removeAttribute('hidden','');
-      sUButton3.setAttribute('hidden','');
-  }
-  else if(password.length === 0){
-    sUButton.setAttribute('hidden','');
-    sUButton2.setAttribute('hidden','');
-    sUButton3.setAttribute('hidden','');
-  }
-  else{
-    
-    sUButton.removeAttribute('hidden','');
-    sUButton2.setAttribute('hidden','');
-    sUButton3.setAttribute('hidden','');
+  if (password.length >= 12 && upperCase === true && digit === true) {
+    sUButton.removeAttribute("hidden", "");
+    sUButton2.removeAttribute("hidden", "");
+    sUButton3.removeAttribute("hidden", "");
+  } else if (
+    password.length > 7 &&
+    password.length < 12 &&
+    upperCase === true
+  ) {
+    sUButton.removeAttribute("hidden", "");
+    sUButton2.removeAttribute("hidden", "");
+    sUButton3.setAttribute("hidden", "");
+  } else if (password.length === 0) {
+    sUButton.setAttribute("hidden", "");
+    sUButton2.setAttribute("hidden", "");
+    sUButton3.setAttribute("hidden", "");
+  } else {
+    sUButton.removeAttribute("hidden", "");
+    sUButton2.setAttribute("hidden", "");
+    sUButton3.setAttribute("hidden", "");
   }
 }
-function upperCaseCheck (pass){
-  return  /[A-Z]/.test(pass);
+function upperCaseCheck(pass) {
+  return /[A-Z]/.test(pass);
 }
 
-function digitCheck(pass){
+function digitCheck(pass) {
   return /[1-9]/.test(pass);
 }
 
-
-
-passwordInput.addEventListener("input", function(){
-    if(loginInput.value.length>0 && passwordInput.value.length>0){
-        checkPassword(passwordInput.value);
-        signUpButton.removeAttribute('disabled','');
-      }
-
-})
+passwordInput.addEventListener("input", function () {
+  if (loginInput.value.length > 0 && passwordInput.value.length > 0) {
+    checkPassword(passwordInput.value);
+    signUpButton.removeAttribute("disabled", "");
+  }
+});
